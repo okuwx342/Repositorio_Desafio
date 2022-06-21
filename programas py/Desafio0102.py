b0e42cf34959df85447ef44706dea25b813e0d50
Eduardo Macena <okuwxzueiro@gmail.com>
2022-06-21 18:01:39 -0300

    Adiciona arquivos de python no repositório

---
+++ programas py/Desafio0102.py
@@ -0,0 +1,4 @@
+dia = input('Qual o dia do seu nascimento? ')
+mes = input('Qual o mês do seu nascimento? ')
+ano = input('Qual o ano do seu nascimento? ')
+print(dia,'/',mes,'/',ano)
---
+++ programas py/Desafio0103.py
@@ -0,0 +1,9 @@
+n1 = float(input('primeiro valor: '))
+n2 = float(input('segundo valor: '))
+soma = n1 + n2
+print('A soma float é: ', soma)
+n3 = int(input('primeiro valor: '))
+n4 = int(input('segundo valor: '))
+ssoma = n3 + n4
+print('A soma int é: ', ssoma)
+
