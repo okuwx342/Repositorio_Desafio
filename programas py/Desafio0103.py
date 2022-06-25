e5179213060eda1e9fbe8ff835fb2854a8970e61
Saiki <GSaiki26@gmail.com>
2022-06-25 15:52:37 -0300

    Added: Saiki notes on challange 0103

--- programas py/Desafio0103.py
+++ programas py/Desafio0103.py
@@ -7,3 +7,23 @@ n4 = int(input('segundo valor: '))
 ssoma = n3 + n4
 print('A soma int é: ', ssoma)
 
+# Note by Saiki: https://www.programiz.com/python-programming/string-interpolation
+# Vc pode fazer as somas sem atribuí-las em variáveis, confira:
+print('A soma dos números é: ' + (n1 + n2))
+
+# Você também pode inseri-las no meio das string, utilizando as f-strings:
+# Apenas coloque o código que vc quer dentro de chaves, e pronto -
+# ele será executado
+print(f'A soma dos números é: {n1 + n2}.')
+
+word = 'tecnologias'
+print(f'Cada dia, as {word} vão dominar o mundo :D')
+
+# Sendo o mais arcaico, vc também pode utilizar a boa e velha 
+# string interpolation com %
+print('Cada dia, as %s vão dominar o mundo :D'%(word))
+
+print('Cada dia, as %(word)s vão dominar o mundo :D')
+
+# Ou por último
+print('Cada dia, as {} vão dominar o mundo :D'.format(word))
